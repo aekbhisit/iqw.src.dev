f62fc89dc88c4a8955ec0ae153ed19ce409a83e5
bankkun73 <sathapornsukrammi@gmail.com>
2017-10-06 13:16:27 +0700

    fix bug modules config
    fix bug modules config

--- app/module/configs/controller.php
+++ app/module/configs/controller.php
@@ -12,17 +12,17 @@ if(isset($_GET['task'])){
 			echo json_encode($data);
 		break;
 		case 'saveConfigsSite':
-			$id = $_POST['configs_id'];
-			$opensite = $_POST['configs_open'];
-			$sitename = $_POST['configs_sitename'];
-			$meta_keywords = $_POST['configs_meta_keywords'];
-			$meta_descritption = $_POST['configs_meta_description'];
-			$favicon_url = $_POST['configs_favicon_url'];
-			$underconstruction_text = $_POST['configs_underconstruction_text'];
-			$background_url = $_POST['configs_background_url'];
+			$id = $oConfigsSite->setInt($_POST['configs_id']);
+			$opensite = $oConfigsSite->setInt($_POST['configs_open']);
+			$sitename = $oConfigsSite->setString($_POST['configs_sitename']);
+			$meta_keywords = $oConfigsSite->setString($_POST['configs_meta_keywords']);
+			$meta_descritption = $oConfigsSite->setString($_POST['configs_meta_description']);
+			$favicon_url = $oConfigsSite->setString($_POST['configs_favicon_url']);
+			$underconstruction_text = $oConfigsSite->setString($_POST['configs_underconstruction_text']);
+			$background_url = $oConfigsSite->setString($_POST['configs_background_url']);
 			$languages = $_POST['configs_languages'];
 			// update site language 
-			$oConfigsSite->updateSiteLanguage ($languages);
+			$oConfigsSite->updateSiteLanguage($languages);
 			if(empty($id)){		
 				$oConfigsSite->insertConfigsSite($opensite,$sitename,$meta_keywords,$meta_descritption,$favicon_url,$underconstruction_text,$background_url);
 			}else{
@@ -35,25 +35,25 @@ if(isset($_GET['task'])){
 			echo json_encode($data);
 		break;
 		case 'saveConfigsEmail':
-			$id = $_POST['configs_id'];
-			$smtp = $_POST['configs_smtp'];
-			$smtp_secure = $_POST['configs_smtp_secure'];
-			$smtp_server = $_POST['configs_smtp_server'];
-			$smtp_port = $_POST['configs_smtp_port'];
-			$smtp_user = $_POST['configs_smtp_user'];
-			$smtp_password= $_POST['configs_smtp_password'];
+			$id = $oConfigsEmail->setInt($_POST['configs_id']);
+			$smtp = $oConfigsEmail->setInt($_POST['configs_smtp']);
+			$smtp_secure = $oConfigsEmail->setString($_POST['configs_smtp_secure']);
+			$smtp_server = $oConfigsEmail->setString($_POST['configs_smtp_server']);
+			$smtp_port = $oConfigsEmail->setString($_POST['configs_smtp_port']);
+			$smtp_user = $oConfigsEmail->setString($_POST['configs_smtp_user']);
+			$smtp_password = $oConfigsEmail->setString($_POST['configs_smtp_password']);
 			if(empty($id)){		
-				$oConfigsEmail->insertConfigsEmail($smtp,$smtp_secure,$smtp_server,$smtp_port,$smtp_user,$smtp_password) ;
+				$oConfigsEmail->insertConfigsEmail($smtp,$smtp_secure,$smtp_server,$smtp_port,$smtp_user,$smtp_password);
 			}else{
 				$oConfigsEmail->updateConfigsEmail($id,$smtp,$smtp_secure,$smtp_server,$smtp_port,$smtp_user,$smtp_password);
 			}
 		break;
 		// for frontend_function 
-		case "front_getSiteConfig" ;
-			$_DATA["configs"] = $oConfigsSite ->getConfigsSiteFront();
+		case "front_getSiteConfig";
+			$_DATA["configs"] = $oConfigsSite->getConfigsSiteFront();
 		break ;
 		case "front_setSiteLanguage";
-			$_SESSION['SITELANGUAGE'] = $_GET['language'];
+			$_SESSION['SITELANGUAGE'] = $oConfigsEmail->setString($_GET['language']);
 			setcookie("SITELANGUAGE",$_SESSION['SITELANGUAGE'],time()+3600*24*30, "/", DOMAIN);	
 		break ;
 	}// switch
