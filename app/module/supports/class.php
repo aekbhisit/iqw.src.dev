8985b17bf9a7cf3e843a9b989d91bb7d99fa9df6
bankkun73 <sathapornsukrammi@gmail.com>
2017-10-17 17:34:18 +0700

    update file master

--- app/module/supports/class.php
+++ app/module/supports/class.php
@@ -1,20 +1,21 @@
 <?php
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 class Supports extends Database {
-	var $module   ;
-	var $security_code ;
+	var $module;
+	var $security_code;
 	public function __construct($module,$table=NULL){
-		$this->module = (empty($table))?$module:$table  ;
-		 parent::__construct((empty($table))?$module:$table );
-		 $this->parent_table  = 'users_categories';
-		 $this->parent_primary_key = 'id';
-		 $this->security_code = SECURITY_CODE ;
+		$this->module = (empty($table))?$module:$table;
+		parent::__construct((empty($table))?$module:$table );
+		$this->parent_table  = 'users_categories';
+		$this->parent_primary_key = 'id';
+		$this->security_code = SECURITY_CODE;
 	}
-	
 	public function siteConfig(){
 		$this->sql = " select * from configs_site  limit 0,1 ";
 		$this->select();
 		return $this->rows[0];
 	}
-	
 }
 ?>
--- app/module/supports/controller.php
+++ app/module/supports/controller.php
@@ -1,30 +1,32 @@
 <?php
-session_start();
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+if ( is_session_started() === FALSE ) { session_start(); }
 $oSupport = new Supports('configs_site');
 $oMailer = new Mailer();
 if(isset($_GET['task'])){
-	$task = $_GET['task'] ;
+	$task = $_GET['task'];
 	switch($task){
 		case 'supportMail':
-				$configs  =  $oSupport->siteConfig() ;
-				if($_POST['client_msg_type']==0){
-					$subj = "แจ้งปัญหา : " .$_POST['client_msg_title'];
-				}else{
-					$subj = "แนะนำ : " .$_POST['client_msg_title'];
-				}
-				
-				$msgs = "\n \n".$_POST['client_msg_text'];
-				$mailTo = 'iquickweb.com@gmail.com' ;
-				$toName ="iQuickWeb" ;
-				$ccTo = NULL ;
-				$bccTo = NULL ;
-				$subject = $subj ;
-				$mailFrom = $configs['site_email'] ;
-				$fromName=$configs['sitename'] ;
-				$msg = $msgs ;
-				$attachments = NULL ;
-				$oMailer-> sendMail($mailTo,$toName,$ccTo,$bccTo,$subject,$mailFrom,$fromName,$msg,$attachments) ;
-			break ;
+			$configs = $oSupport->siteConfig();
+			if($_POST['client_msg_type']==0){
+				$subj = "แจ้งปัญหา : " .$_POST['client_msg_title'];
+			}else{
+				$subj = "แนะนำ : " .$_POST['client_msg_title'];
+			}
+			$msgs = "\n \n".$_POST['client_msg_text'];
+			$mailTo = 'iquickweb.com@gmail.com';
+			$toName = "iQuickWeb";
+			$ccTo = NULL;
+			$bccTo = NULL;
+			$subject = $subj;
+			$mailFrom = $configs['site_email'];
+			$fromName = $configs['sitename'];
+			$msg = $msgs;
+			$attachments = NULL;
+			$oMailer->sendMail($mailTo,$toName,$ccTo,$bccTo,$subject,$mailFrom,$fromName,$msg,$attachments);
+		break;
 	}// switch
 }// if isset
 ?>
